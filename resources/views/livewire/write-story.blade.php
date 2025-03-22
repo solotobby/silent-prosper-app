31814d91bb12a882bdceea45252497d56f91a09e
Eclatspad <eclatspad@gmail.com>
2025-03-22 15:54:31 +0000

    changed image location

--- resources/views/livewire/write-story.blade.php
+++ resources/views/livewire/write-story.blade.php
@@ -20,6 +20,27 @@
                         </div>
                     @endif
 
+                    <div class="row mb-4">
+                      <div class="col-xl-6">
+                        <label class="form-label" for="dm-post-add-image">Featured Image</label>
+                        <input class="form-control" type="file" wire:model="img" id="dm-post-add-image">
+                        @error('img') <span class="text-danger">{{ $message }}</span> @enderror
+  
+                         @if ($img)
+                          <div class="mt-2">
+                            @if (is_object($img) && method_exists($img, 'temporaryUrl'))
+                              <!-- New file uploaded: display the temporary preview -->
+                              <img src="{{ $img->temporaryUrl() }}" alt="Image Preview" class="img-fluid">
+                            @else
+                              <!-- Existing image: display the stored image (adjust the path as needed) -->
+                              <img src="{{  $img }}" alt="Current Image" class="img-responsive img-thumbnail" sizes="50">
+                            @endif
+                          </div>
+                        @endif 
+  
+                      </div>
+                    </div>
+
                   <div class="mb-4">
                     <label class="form-label" for="dm-post-add-title">Title</label>
                     <input type="text" class="form-control" id="dm-post-add-title" wire:model="title" placeholder="Enter a title..">
@@ -74,26 +95,7 @@
                     </div>
                   </div>
 
-                  <div class="row mb-4">
-                    <div class="col-xl-6">
-                      <label class="form-label" for="dm-post-add-image">Featured Image</label>
-                      <input class="form-control" type="file" wire:model="img" id="dm-post-add-image">
-                      @error('img') <span class="text-danger">{{ $message }}</span> @enderror
-
-                       @if ($img)
-                        <div class="mt-2">
-                          @if (is_object($img) && method_exists($img, 'temporaryUrl'))
-                            <!-- New file uploaded: display the temporary preview -->
-                            <img src="{{ $img->temporaryUrl() }}" alt="Image Preview" class="img-fluid">
-                          @else
-                            <!-- Existing image: display the stored image (adjust the path as needed) -->
-                            <img src="{{  $img }}" alt="Current Image" class="img-responsive img-thumbnail" sizes="50">
-                          @endif
-                        </div>
-                      @endif 
-
-                    </div>
-                  </div>
+                  
                
                 </div>
               </div>
